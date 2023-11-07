f3575f4cb50230ab9fabaf957b4ad6ab7f066db7
NayefMazen <nradwi@starlink.qa>
2023-11-07 23:10:54 +0300

    sizes

--- src/lib/components/PrimaryButton.svelte
+++ src/lib/components/PrimaryButton.svelte
@@ -4,19 +4,24 @@
     export let disabled: boolean | undefined = false;
     export let loading: boolean | undefined = false;
     export let colorClass: string | undefined = "primary";
+    export let type: "button" | "submit" | "reset" | null | undefined =
+        "button";
+
+    export let size: "m" | "s" | "l" | "xs" | "xl" | undefined = "m";
 </script>
 
-<button class={colorClass} {disabled} on:click={onClick}>
-    {label}
+<button {type} class="{colorClass} {size}" {disabled} on:click={onClick}>
+    <span>{label}</span>
     {#if loading}
-        <span class="ml-2">
-            <i class="fas fa-spinner fa-spin" />
-        </span>
+        <img src="oval.svg" alt="loading spinner" height="24" width="24" />
     {/if}
 </button>
 
 <style>
     .button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
         padding: 0.5rem 1rem;
         font-size: 1rem;
         font-weight: 500;
@@ -29,5 +34,60 @@
 
     .primary:hover {
         background-color: var(--primary-hover);
+    }
+
+    .m {
+        border-radius: 0.25rem;
+        font-size: 1rem;
+    }
+
+    .m img {
+        margin-left: 0.5rem;
+        height: 16px;
+        width: 16px;
+    }
+
+    .s {
+        border-radius: 0.125rem;
+        font-size: 0.875rem;
+    }
+
+    .s img {
+        margin-left: 0.5rem;
+        height: 12px;
+        width: 12px;
+    }
+
+    .l {
+        border-radius: 0.5rem;
+        font-size: 1.25rem;
+    }
+
+    .l img {
+        margin-left: 0.5rem;
+        height: 20px;
+        width: 20px;
+    }
+
+    .xs {
+        border-radius: 0.125rem;
+        font-size: 0.75rem;
+    }
+
+    .xs img {
+        margin-left: 0.5rem;
+        height: 10px;
+        width: 10px;
+    }
+
+    .xl {
+        border-radius: 0.5rem;
+        font-size: 1.5rem;
+    }
+
+    .xl img {
+        margin-left: 0.5rem;
+        height: 24px;
+        width: 24px;
     }
 </style>
