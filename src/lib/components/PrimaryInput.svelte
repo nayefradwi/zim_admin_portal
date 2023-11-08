b81524e07a30c4cd1b0b0020c381258128e4374f
NayefMazen <nradwi@starlink.qa>
2023-11-08 14:54:59 +0300

    implemented good error message style

--- src/lib/components/PrimaryInput.svelte
+++ src/lib/components/PrimaryInput.svelte
@@ -2,16 +2,28 @@
     export let placeholder: string | undefined = "";
     export let type: "email" | "password" | "text" | "number" | undefined =
         "text";
-    export let inputColor: "input-primary" | "input-secondary" | undefined =
+    export let inputColor: "input-primary" | "input-secondary" =
         "input-primary";
     export let size: "input-sm" | "input-lg" | "input-xs" | "" | undefined = "";
     export let isFilled: boolean | undefined = false;
+    export let errorMessage: string | undefined = undefined;
+
+    function getColor(): string | null {
+        if (!!errorMessage) return "input-error";
+        if (isFilled) return inputColor;
+        return null;
+    }
 </script>
 
-<input
-    {type}
-    {placeholder}
-    class="input {isFilled
-        ? inputColor
-        : null} [&:not(:focus)]:input-bordered w-full focus:{inputColor} focus:border-0 {size}"
-/>
+<div class="form-control w-full max-w-s">
+    <input
+        {type}
+        {placeholder}
+        class="input {getColor()} [&:not(:focus)]:input-bordered w-full focus:{inputColor} focus:border-0 {size}"
+    />
+    {#if errorMessage}
+        <span class="label">
+            <span class="label-text-alt text-error">{errorMessage}</span>
+        </span>
+    {/if}
+</div>
