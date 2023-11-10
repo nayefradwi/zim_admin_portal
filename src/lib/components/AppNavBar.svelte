c4a8cadf64b4291250b853d728ab5724d831bd3c
NayefMazen <nradwi@starlink.qa>
2023-11-10 20:47:49 +0300

    added warehouse logic

---
+++ src/lib/components/AppNavBar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { navigate } from "svelte-routing";
+    import { DASHBOARD_ROUTE, WAREHOUSE_SELECT_ROUTE } from "../routes";
+    import { setWarehouseHeader } from "../../data";
+    function changeWarehouse() {
+        setWarehouseHeader(null);
+        navigate(WAREHOUSE_SELECT_ROUTE);
+    }
+    function goToDashboard() {
+        navigate(DASHBOARD_ROUTE);
+    }
+</script>
+
+<div class="navbar bg-base-100 shadow">
+    <div class="flex-1">
+        <button
+            class="btn btn-ghost normal-case text-xl no-animation"
+            on:click={goToDashboard}>Zanobia Inventory Manager</button
+        >
+    </div>
+    <div class="flex-none">
+        <button class="btn btn-outline btn-error" on:click={changeWarehouse}>
+            Change Warehouse
+        </button>
+    </div>
+</div>
--- src/lib/pages/home/Home.svelte
+++ src/lib/pages/home/Home.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+    import AppNavBar from "../../components/AppNavBar.svelte";
     import AppNavDrawer from "../../components/AppNavDrawer.svelte";
 </script>
 
 <main class="h-full">
     <!-- header -->
-
+    <AppNavBar />
+    <div class="my-2" />
     <!-- drawer -->
     <AppNavDrawer />
     <!-- body -->
