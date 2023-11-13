c68791940814744f54d592beb2499254233e4a41
NayefMazen <nradwi@starlink.qa>
2023-11-13 18:02:09 +0300

    added action buttons

--- src/lib/pages/home/inventory/BatchTableRow.svelte
+++ src/lib/pages/home/inventory/BatchTableRow.svelte
@@ -2,6 +2,7 @@
     import type { DateTime } from "luxon";
     import type { Batch } from "../../../../data";
     import { parseDate, getDifferenceInDays } from "../../../utils/functions";
+    import { EyeIcon, MinusIcon, PlusIcon } from "svelte-feather-icons";
     export let batchItem: Batch;
     const totalWorth = batchItem.productVariant.price * batchItem.quantity;
     let expiresAt: DateTime = parseDate(batchItem.expiresAt);
@@ -22,5 +23,20 @@
         <th class={diffInDays < 15 ? "text-error" : null}
             >{expiresAt.toLocaleString()}</th
         >
+        <td>
+            <button class="btn btn-ghost no-animation btn-xs p-0 m-0">
+                <!-- TODO increment batch -->
+                <PlusIcon />
+            </button>
+            <button class="btn btn-ghost no-animation btn-xs p-0 m-0">
+                <!-- TODO view variant -->
+                <EyeIcon />
+            </button>
+
+            <button class="btn btn-ghost no-animation btn-xs p-0 m-0">
+                <!-- TODO decrement batch -->
+                <MinusIcon />
+            </button>
+        </td>
     {/if}
 </tr>
