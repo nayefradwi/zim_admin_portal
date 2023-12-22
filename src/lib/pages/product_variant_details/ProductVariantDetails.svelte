895faa5427601f32e5fd140b80094093c9eb49d2
Nayef Radwi <45208221+nayefradwi@users.noreply.github.com>
2023-12-22 20:28:48 +0300

    loading product variant

--- src/lib/pages/product_variant_details/ProductVariantDetails.svelte
+++ src/lib/pages/product_variant_details/ProductVariantDetails.svelte
@@ -1,8 +1,42 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import {
+    ProductRepo,
+    getResponse,
+    type ProductVariant,
+    type ResponseHandlerData,
+  } from "../../../data";
+  import toast from "svelte-french-toast";
   export let productVariantId: string;
+
+  let isLoading: boolean = false;
+  let productVariant: ProductVariant | undefined = undefined;
+  function loadProductVariantDetails() {
+    isLoading = true;
+    const details: ResponseHandlerData<ProductVariant> = {
+      call: () => ProductRepo.getProductVariantById(productVariantId),
+      onSuccess: (data) => {
+        isLoading = false;
+        productVariant = data;
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    };
+    getResponse<ProductVariant>(details);
+  }
+
+  onMount(() => {
+    loadProductVariantDetails();
+  });
 </script>
 
 <div>
-  <h1>Product Variant Details</h1>
-  <p>Product Variant Id: {productVariantId}</p>
+  {#if isLoading}
+    <div>Loading...</div>
+  {:else if !productVariant}
+    <div>Product variant not found</div>
+  {:else}
+    <div>{productVariant.name}</div>
+  {/if}
 </div>
