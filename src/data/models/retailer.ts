b3963a9f9cb5368f369f6014861ca1fa46eeda01
Nayef Radwi <45208221+nayefradwi@users.noreply.github.com>
2023-12-27 18:36:41 +0300

    implemented interface

--- src/data/models/retailer.ts
+++ src/data/models/retailer.ts
@@ -1,3 +1,5 @@
+import type { ProductVariant, Unit } from ".";
+
 export interface Retailer {
   id: number;
   name: string;
@@ -10,7 +12,19 @@ export interface RetailerContact {
   id: number;
   name: string;
   position: string;
-  email: string;
+  email?: string;
   phone: string;
-  website: string;
+  website?: string;
+}
+
+export interface RetailerBatch {
+  id: number;
+  retailerId: number;
+  sku: string;
+  quantity: number;
+  expiresAt: string;
+  productVariant: ProductVariant;
+  unit: Unit;
+  productName: string;
+  retailerName: string;
 }
--- src/data/repo/retailer.ts
+++ src/data/repo/retailer.ts
@@ -1,3 +1,132 @@
-export interface IRetailerRepo {}
+import {
+  apiClient,
+  type PaginatedModel,
+  type PaginationQuery,
+  type Retailer,
+  type RetailerBatch,
+} from "..";
 
-export const RetailerRepo: IRetailerRepo = {};
+export interface IRetailerRepo {
+  createRetailer(input: CreateRetailerInput): Promise<void>;
+  addRetailerContact(
+    id: number,
+    input: CreateRetailerContactInput
+  ): Promise<void>;
+  updateRetailer(input: UpdateRetailerInput): Promise<void>;
+  getRetailer(id: number): Promise<Retailer>;
+  getRetailers(): Promise<PaginatedModel<Retailer>>;
+  deleteRetailer(id: number): Promise<void>;
+  deleteRetailerContact(id: number): Promise<void>;
+  incrementRetailerBatch(input: ModifyRetailerBatchInput): Promise<void>;
+  decrementRetailerBatch(input: ModifyRetailerBatchInput): Promise<void>;
+  getBatchesOfRetailer(
+    retailerId: number,
+    query?: PaginationQuery
+  ): Promise<RetailerBatch[]>;
+  getBatches(query?: PaginationQuery): Promise<RetailerBatch[]>;
+}
+
+export interface CreateRetailerInput {
+  name: string;
+  lat: number;
+  lng: number;
+  contacts: CreateRetailerContactInput[];
+}
+
+export interface CreateRetailerContactInput {
+  name: string;
+  position: string;
+  email?: string;
+  phone: string;
+  website?: string;
+}
+
+export interface UpdateRetailerInput {
+  name: string;
+  lat: number;
+  lng: number;
+}
+
+export interface ModifyRetailerBatchInput {
+  id?: number;
+  quantity: number;
+  unitId: number;
+  sku: string;
+  retailerId: number;
+  reason: string;
+}
+
+export const RetailerRepo: IRetailerRepo = {
+  createRetailer: async (input: CreateRetailerInput): Promise<void> => {
+    const response = await apiClient.post("/retailers", input);
+    return response.data;
+  },
+  addRetailerContact: async (
+    id: number,
+    input: CreateRetailerContactInput
+  ): Promise<void> => {
+    const response = await apiClient.post(`/retailers/${id}/contact`, input);
+    return response.data;
+  },
+  updateRetailer: async (input: UpdateRetailerInput): Promise<void> => {
+    const response = await apiClient.put(`/retailers`, input);
+    return response.data;
+  },
+  getRetailer: async (id: number): Promise<Retailer> => {
+    const response = await apiClient.get<Retailer>(`/retailers/${id}`);
+    return response.data;
+  },
+  getRetailers: async (): Promise<PaginatedModel<Retailer>> => {
+    const response = await apiClient.get<PaginatedModel<Retailer>>(
+      `/retailers`
+    );
+    return response.data;
+  },
+  deleteRetailer: async (id: number): Promise<void> => {
+    const response = await apiClient.delete(`/retailers/${id}`);
+    return response.data;
+  },
+  deleteRetailerContact: async (id: number): Promise<void> => {
+    const response = await apiClient.delete(`/retailers/contact/${id}`);
+    return response.data;
+  },
+  incrementRetailerBatch: async (
+    input: ModifyRetailerBatchInput
+  ): Promise<void> => {
+    const response = await apiClient.post(
+      `/retailers/batches/batch/stock`,
+      input
+    );
+    return response.data;
+  },
+  decrementRetailerBatch: async (
+    input: ModifyRetailerBatchInput
+  ): Promise<void> => {
+    const response = await apiClient.delete(`/retailers/batches/batch/stock`, {
+      data: input,
+    });
+    return response.data;
+  },
+  getBatchesOfRetailer: async (
+    retailerId: number,
+    query?: PaginationQuery
+  ): Promise<RetailerBatch[]> => {
+    const response = await apiClient.get<RetailerBatch[]>(
+      `/retailers/${retailerId}/batches`,
+      {
+        params: query,
+      }
+    );
+    return response.data;
+  },
+
+  getBatches: async (query?: PaginationQuery): Promise<RetailerBatch[]> => {
+    const response = await apiClient.get<RetailerBatch[]>(
+      `/retailers/batches`,
+      {
+        params: query,
+      }
+    );
+    return response.data;
+  },
+};
