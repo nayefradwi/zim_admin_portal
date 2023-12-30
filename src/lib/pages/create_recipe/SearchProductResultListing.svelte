2b229c80db33a1c4d9b42f27625cfed8355f3f68
Nayef Radwi <45208221+nayefradwi@users.noreply.github.com>
2023-12-30 13:44:37 +0300

    implemented product listing in create recipe

--- src/lib/pages/create_recipe/SearchProductResultListing.svelte
+++ src/lib/pages/create_recipe/SearchProductResultListing.svelte
@@ -4,15 +4,19 @@
   export let productVariants: ProductVariant[];
 </script>
 
-<div>
+<div class="space-y-2 mt-2">
   {#each productVariants as variant}
     <!-- product name -->
-    <div>
-      {variant.productName}
-      {variant.name}
-      {variant.sku}
-      {variant.standardUnit?.name}
-      {variant.price}
+    <div class="card bg-base-100 shadow">
+      <div class="card-body p-4 items-start">
+        <div class="card-title">{variant.productName} / {variant.name}</div>
+        <div class="card-title">{variant.sku}</div>
+        <p>product variant standard unit: {variant.standardUnit?.name}</p>
+        <p>product variant standard unit cost: {variant.price}</p>
+        <div class="card-actions justify-end self-end">
+          <button class="btn btn-primary">Add to recipe</button>
+        </div>
+      </div>
     </div>
     <!-- variant name -->
     <!-- unit -->
