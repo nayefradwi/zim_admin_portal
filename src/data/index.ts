7f1e83708562c66d3fb993adbdb921c1866c624c
Nayef Radwi <45208221+nayefradwi@users.noreply.github.com>
2023-12-31 20:01:24 +0300

    fixed import issue

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,5 @@
-export * from './models/index';
-export * from './utils/index';
-export * from './repo/index';
-export * from './api_client';
-export * from './session_storage';
-export * from './local_storage'
-
+export * from "./models/index";
+export * from "./utils/index";
+export * from "./repo/index";
+export * from "./api_client";
+export * from "./local_storage";
