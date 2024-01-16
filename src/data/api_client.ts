a8ead1396f8d844813b3435932d624303ad44018
Nayef Radwi <45208221+nayefradwi@users.noreply.github.com>
2024-01-16 21:21:05 +0300

    added api url env variable

--- src/data/api_client.ts
+++ src/data/api_client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ClientError } from "./models/error";
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.API_URL || "http://localhost:3000",
   headers: {},
 });
 
