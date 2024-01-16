2b91a05a3bfd7caa2f7f1450c95c4b95233bcb6d
Nayef Radwi <45208221+nayefradwi@users.noreply.github.com>
2024-01-16 21:41:59 +0300

    for now hard coding backend

--- src/data/api_client.ts
+++ src/data/api_client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ClientError } from "./models/error";
 
 export const apiClient = axios.create({
-  baseURL: process.env.API_URL || "http://localhost:3000",
+  baseURL: "https://zanobiainventorymanager-staging.up.railway.app",
   headers: {},
 });
 
